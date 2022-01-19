ea230b1e3931119606eef4ffe7dbd62cf4e64f10
yancy <yyancy517@gmail.com>
2022-01-19 18:11:44 +0800

    commit 1 chapter exercises

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -15,18 +15,20 @@ main(int argc, char **argv)
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port   = htons(13);	/* daytime server */
+	servaddr.sin_port   = htons(9999);	/* daytime server */
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
 		err_quit("inet_pton error for %s", argv[1]);
 
 	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
 		err_sys("connect error");
-
+  int counter = 0;
 	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
+    counter++;
 		recvline[n] = 0;	/* null terminate */
 		if (fputs(recvline, stdout) == EOF)
 			err_sys("fputs error");
 	}
+  printf("counter is %d\n", counter);
 	if (n < 0)
 		err_sys("read error");
 
--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -14,7 +14,7 @@ main(int argc, char **argv)
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(13);	/* daytime server */
+	servaddr.sin_port        = htons(9999);	/* daytime server */
 
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
@@ -25,8 +25,11 @@ main(int argc, char **argv)
 
         ticks = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        Write(connfd, buff, strlen(buff));
-
+        // Write(connfd, buff, strlen(buff));
+        for (int i = 0; i < strlen(buff); ++i) {
+          if (write(connfd,&buff[i], 1) != 1)
+            err_sys("write error");
+        }
 		Close(connfd);
 	}
 }
